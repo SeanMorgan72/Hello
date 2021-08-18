daa1955927d3c2040d1954fd6b6f4fbb6acc1b76
Sean Morgan <scm0323@att.net>
2021-08-18 13:01:35 -0500

    changed Hello World

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hello();
+        }
+
+        public void Hello()
+        {
+            Console.WriteLine("What is your name?");
+            var name = Console.ReadLine();
+            Console.WriteLine("What is your favorite food to eat?");
+            var food = Console.ReadLine();
+            Console.WriteLine("Hello, " + name + "!");
+            Console.WriteLine("Your favorite food to eat is " + food + ".");
+        }
+    }
+}
